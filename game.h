edeb563e6a4d221fb421f0f32102dba9ffb4f466
Bryant Wu <bryantwu2@gmail.com>
2024-09-10 01:09:29 -0700

    Updated main.cpp, game.h, game.exe
    added 2 classes, Food and Snake
    Food 
    - learned how to import images and draw textures on the screen (although not using since I couldn't find a good resizable image, might return to this later) 
    - randomized food output on the screen 🍎🍎🍎
    Snake
    - using deques to draw the snake (learned this from CSCI 240 class) 
    - no movement yet , only drawing to the screen 🐍🐍🐍

--- game.h
+++ game.h
@@ -1,2 +1,5 @@
 static constexpr int InitialWidth = 750;
 static constexpr int InitialHeight = 750;
+
+#define CELLSIZE 30
+#define CELLCOUNT 25
--- main.cpp
+++ main.cpp
@@ -1,20 +1,66 @@
 #include "raylib.h"
 #include "game.h"
-#include <iostream>
+#include <deque>
 
-Color green = {173, 204, 96, 255};
-Color darkGreen = {43, 51, 24, 255};
+Color lightGreen = {155, 188, 15, 255};
+Color green = {48, 98, 48, 255};
+Color darkGreen = {15, 56, 15, 255};
+Color red = {175, 0, 0, 255};
+
+class Snake
+{
+public:
+    std::deque<Vector2> body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+
+    void draw()
+    {
+        for (unsigned int i = 0; i < body.size(); i++)
+        {
+            float x = body[i].x;
+            float y = body[i].y;
+            Rectangle segment = Rectangle{x * CELLSIZE, y * CELLSIZE, (float)CELLSIZE, (float)CELLSIZE};
+            DrawRectangleRounded(segment, 0.5, 6, green);
+        }
+    }
+};
+
+class Food
+{
+public:
+    Vector2 position;
+
+    Food()
+    {
+        position = GenerateRandomPos();
+    }
+
+    void draw()
+    {
+        DrawRectangle(position.x * CELLSIZE, position.y * CELLSIZE, CELLSIZE, CELLSIZE, red); // x, y, width, height, color
+    }
+
+    Vector2 GenerateRandomPos()
+    {
+        float x = GetRandomValue(0, CELLCOUNT - 1);
+        float y = GetRandomValue(0, CELLCOUNT - 1);
+        return Vector2{x, y};
+    }
+};
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Starting game..." << std::endl;
-    InitWindow(InitialWidth, InitialHeight, "Snake"); //width, height, window name 
+    InitWindow(InitialWidth, InitialHeight, "Snake"); // width, height, window name
     SetTargetFPS(60);
+
+    Food food = Food();
+    Snake snake = Snake();
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(green);
+        ClearBackground(lightGreen);
+        food.draw();
+        snake.draw();
 
         EndDrawing();
     }
